00d80fddf31240a34567c87e001ca397f85281f0
Simeon Widdis <sawiddis@gmail.com>
2024-01-12 18:16:03 -0800

    Add pytest fixtures for game entities

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,4 +1,6 @@
 import pathlib
+import random
+import string
 import tempfile
 
 import pytest
@@ -6,25 +8,29 @@ import pytest
 from src.game import WordDebtGame, WordDebtPlayer
 
 
-def test_game_initializes():
+@pytest.fixture
+def game():
     state_file_path = tempfile.mkstemp(suffix=".json")
-    game = WordDebtGame(pathlib.Path(state_file_path[1]))
+    return WordDebtGame(pathlib.Path(state_file_path[1]))
+
+
+@pytest.fixture
+def player():
+    user_id = str(random.randint(10**7, 10**8 - 1))
+    user_name = "".join(random.choice(string.ascii_lowercase) for _ in range(8))
+    return WordDebtPlayer(user_id, user_name)
+
+
+def test_game_initializes(game):
     assert game._state == {}
 
 
-def test_game_registers_player():
-    state_file_path = tempfile.mkstemp(suffix=".json")
-    game = WordDebtGame(pathlib.Path(state_file_path[1]))
-    player = WordDebtPlayer("123", "Test Player", 100000)
+def test_game_registers_player(game, player):
     game.register_player(player)
-    print(game._state)
     assert game._state[player.user_id] == player
 
 
-def test_game_refuses_duplicate_registration():
-    state_file_path = tempfile.mkstemp(suffix=".json")
-    game = WordDebtGame(pathlib.Path(state_file_path[1]))
-    player = WordDebtPlayer("123", "Test Player", 100000)
+def test_game_refuses_duplicate_registration(game, player):
     game.register_player(player)
     with pytest.raises(ValueError):
         game.register_player(player)
