be65c2c9800058fd7d5bf920119fcb7d97398541
Simeon Widdis <sawiddis@gmail.com>
2024-01-13 19:25:23 -0800

    Add token reading

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
+import os
 import pathlib
 
 from game import WordDebtGame
 
+TOKEN_PATH = pathlib.Path("data/TOKEN")
+DB_PATH = pathlib.Path("data/prod.json")
+
+
+def get_token():
+    if not os.path.exists(TOKEN_PATH):
+        raise FileNotFoundError(
+            f"Missing TOKEN file. The bot requires a Discord bot token at `{TOKEN_PATH}` to run."
+        )
+    with open(TOKEN_PATH, "r") as token_file:
+        return token_file.read()
+
+
 if __name__ == "__main__":
-    game = WordDebtGame(pathlib.Path("data/prod.json"))
+    token = get_token()
+    game = WordDebtGame(DB_PATH)
