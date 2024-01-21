15346f4bd619777cb4ea14ba76679a17fdfdb520
Simeon Widdis <sawiddis@gmail.com>
2024-01-21 15:50:20 -0800

    Add basic replay commands

--- src/journal_replay/main.py
+++ src/journal_replay/main.py
@@ -1,6 +1,10 @@
 import json
 import pathlib
+import tempfile
 from pprint import pprint
+
+from word_debt_bot.game import WordDebtGame
+from word_debt_bot.game.player import WordDebtPlayer
 
 
 def read_journal_file(journal_file: pathlib.Path):
@@ -16,8 +20,33 @@ def get_journal(
     return sorted(base_journal + edits_journal, key=lambda x: x["time"])
 
 
+def apply_entry(entry, game):
+    if entry["command"] == "register":
+        if entry["user"] not in game._state:
+            # TODO handle display names
+            # Probably need to pull from existing state to get config options
+            game.register_player(
+                WordDebtPlayer(
+                    user_id=entry["user"], display_name="unknown", word_debt=10000
+                )
+            )
+    if entry["command"] == "log":
+        game.submit_words(entry["user"], entry["words"])
+
+
+def replay_journal(journal: list[dict]) -> WordDebtGame:
+    temp_state = tempfile.mkstemp()
+    game = WordDebtGame(pathlib.Path(temp_state[1]))
+
+    for entry in journal:
+        apply_entry(entry, game)
+
+    return game
+
+
 if __name__ == "__main__":
     journal = get_journal(
         pathlib.Path("data/journal.ndjson"), pathlib.Path("data/edits.ndjson")
     )
-    pprint(journal)
+    game = replay_journal(journal)
+    pprint(game._state)
