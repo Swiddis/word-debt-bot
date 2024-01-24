31dbcdbc069eed2e817d2370e10f8118e1f199c9
Simeon Widdis <sawiddis@gmail.com>
2024-01-23 22:58:11 -0800

    Use bonus genre in applying bonus

--- tests/test_store.py
+++ tests/test_store.py
@@ -39,5 +39,14 @@ def test_game_handles_bonus_genre(
 ):
     game_state.register_player(player)
     game_state.add_bonus_genre("fantasy")
-    game_state.submit_words(player.user_id, 2000)
+    game_state.submit_words(player.user_id, 2000, "fantasy")
     assert game_state._state.users[player.user_id].cranes == 8
+
+
+def test_game_handles_bonus_genre_mismatch(
+    game_state: game.WordDebtGame, player: game.WordDebtPlayer
+):
+    game_state.register_player(player)
+    game_state.add_bonus_genre("fantasy")
+    game_state.submit_words(player.user_id, 2000, "sci-fi")
+    assert game_state._state.users[player.user_id].cranes == 4
