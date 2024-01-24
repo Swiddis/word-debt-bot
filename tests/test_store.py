03f4e50324468972d57a9f197a8578c942c9ab6f
Simeon Widdis <sawiddis@gmail.com>
2024-01-23 22:15:50 -0800

    Add ability to spend cranes

--- tests/test_store.py
+++ tests/test_store.py
@@ -3,6 +3,37 @@ from word_debt_bot import game
 from .fixtures import *
 
 
+def test_game_removes_spent_cranes(
+    game_state: game.WordDebtGame, player: game.WordDebtPlayer
+):
+    player.cranes = 50
+    game_state.register_player(player)
+
+    game_state.spend_cranes(player.user_id, 25)
+
+    assert game_state._state.users[player.user_id].cranes == 25
+
+
+def test_game_blocks_overspending(
+    game_state: game.WordDebtGame, player: game.WordDebtPlayer
+):
+    player.cranes = 50
+    game_state.register_player(player)
+
+    with pytest.raises(ValueError):
+        game_state.spend_cranes(player.user_id, 100)
+
+
+def test_game_blocks_negative_spending(
+    game_state: game.WordDebtGame, player: game.WordDebtPlayer
+):
+    player.cranes = 50
+    game_state.register_player(player)
+
+    with pytest.raises(ValueError):
+        game_state.spend_cranes(player.user_id, -50)
+
+
 def test_game_handles_bonus_genre(
     game_state: game.WordDebtGame, player: game.WordDebtPlayer
 ):
