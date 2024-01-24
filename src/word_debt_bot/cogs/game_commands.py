4d7520d5e08ac4b8f1e2e725b1ba541519dfbd8f
Simeon Widdis <sawiddis@gmail.com>
2024-01-23 22:45:03 -0800

    Add info command

--- src/word_debt_bot/cogs/game_commands.py
+++ src/word_debt_bot/cogs/game_commands.py
@@ -53,6 +53,19 @@ class GameCommands(commands.Cog, name="Core Gameplay Module"):
         except ValueError:
             await ctx.send("Already registered!")
 
+    @commands.command(name="info")
+    async def info(self, ctx):
+        if not self.game:
+            await ctx.send("Game not loaded. (Yell at Toast!)")
+            return
+        try:
+            player = self.game.get_player(str(ctx.author.id))
+            await ctx.send(
+                f"Info for {player.display_name}:\nDebt: {player.word_debt}\nCranes: {player.cranes}"
+            )
+        except KeyError:
+            await ctx.send("Not registered! `.register`")
+
     @commands.command(name="log")
     async def log(self, ctx, words: int, genre: typing.Optional[str] = None):
         if not self.game:
