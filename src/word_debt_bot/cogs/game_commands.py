fc7ec7184f0b9992b54b6ce7384a402d8e1d7ca0
Simeon Widdis <sawiddis@gmail.com>
2024-01-26 17:56:19 -0800

    Split buy commands into subcommands

--- src/word_debt_bot/cogs/game_commands.py
+++ src/word_debt_bot/cogs/game_commands.py
@@ -103,50 +103,56 @@ class GameCommands(commands.Cog, name="Core Gameplay Module"):
             await ctx.send(f"Error: {str(err)}")
 
     @commands.command(name="buy")
-    async def buy(self, ctx, item: str, *, args):
+    async def buy(self, ctx, item: str, *, args=""):
         try:
             match item.lower().strip():
                 case "bonus genre":
-                    if len(args) == 0:
-                        await ctx.send("Must specify a genre!")
-                        return
-                    genre = " ".join(args.split()).lower()
-                    user_id = str(ctx.author.id)
-                    self.game.spend_cranes(user_id, 200)
-                    self.game.add_bonus_genre(genre)
-                    self.journal(
-                        {
-                            "command": "buy",
-                            "user": user_id,
-                            "item": "bonus genre",
-                            "genre": genre,
-                        }
-                    )
-                    await ctx.send(f"New bonus genre active: {genre}")
+                    await self.buy_bonus_genre(ctx, args)
                 case "debt increase":
-                    target_id = re.match(r"<@(\d+)>", args)
-                    if not target_id:
-                        await ctx.send("Must specify a target player!")
-                        return
-                    target_player = self.game.get_player(target_id.group(1))
-                    if not target_player:
-                        await ctx.send("Target player is not registered!")
-                        return
-                    user_id = str(ctx.author.id)
-                    self.game.spend_cranes(user_id, 20)
-                    self.game.add_debt(target_player.user_id, 10000)
-                    self.journal(
-                        {
-                            "command": "buy",
-                            "user": user_id,
-                            "item": "debt increase",
-                            "target": target_player.user_id,
-                        }
-                    )
-                    await ctx.send(
-                        f"Increased {target_player.display_name}'s debt by 10,000! How mean..."
-                    )
+                    await self.buy_debt_increase(ctx, args)
                 case _:
                     await ctx.send("Invalid store item")
         except ValueError as err:
             await ctx.send(f"Error: {err}")
+
+    async def buy_bonus_genre(self, ctx, args):
+        if len(args) == 0:
+            await ctx.send("Must specify a genre!")
+            return
+        genre = " ".join(args.split()).lower()
+        user_id = str(ctx.author.id)
+        self.game.spend_cranes(user_id, 200)
+        self.game.add_bonus_genre(genre)
+        self.journal(
+            {
+                "command": "buy",
+                "user": user_id,
+                "item": "bonus genre",
+                "genre": genre,
+            }
+        )
+        await ctx.send(f"New bonus genre active: {genre}")
+
+    async def buy_debt_increase(self, ctx, args):
+        target_id = re.match(r"<@(\d+)>", args)
+        if not target_id:
+            await ctx.send("Must specify a target player!")
+            return
+        target_player = self.game.get_player(target_id.group(1))
+        if not target_player:
+            await ctx.send("Target player is not registered!")
+            return
+        user_id = str(ctx.author.id)
+        self.game.spend_cranes(user_id, 20)
+        self.game.add_debt(target_player.user_id, 10000)
+        self.journal(
+            {
+                "command": "buy",
+                "user": user_id,
+                "item": "debt increase",
+                "target": target_player.user_id,
+            }
+        )
+        await ctx.send(
+            f"Increased {target_player.display_name}'s debt by 10,000! How mean..."
+        )
