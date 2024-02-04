fe8ae62101571261cfac18755af7de601f248030
Simeon Widdis <sawiddis@gmail.com>
2024-02-04 00:07:11 -0800

    Split up err handler checks

--- src/word_debt_bot/cogs/cmd_err_handler.py
+++ src/word_debt_bot/cogs/cmd_err_handler.py
@@ -24,81 +24,74 @@ class CmdErrHandler(commands.Cog, name="Command Error Handler"):
         if hasattr(ctx.command, "on_error"):
             return
 
-        # Prevents responses to commands that the bot does not have
-        if isinstance(err, commands.CommandNotFound):
-            return
+        match err:
+            case commands.CommandNotFound():
+                return
+            case commands.DisabledCommand():
+                await ctx.send(f"Sorry, {ctx.command} is currently disabled.\n")
+            case commands.MissingPermissions():
+                await ctx.send(
+                    f"Sorry, you do not have the required permissions for {ctx.command}.\n"
+                )
+            case commands.MissingRole():
+                await ctx.send(
+                    f"Sorry, you do not have the required role for {ctx.command}.\n"
+                )
+            case commands.MissingAnyRole():
+                await ctx.send(
+                    f"Sorry, you do not have all the required roles for {ctx.command}.\n"
+                )
+            case commands.BadArgument():
+                await ctx.send(
+                    f"Invalid inputs were supplied for {ctx.command}!\n"
+                    f"For more information type `.help {ctx.command}`"
+                )
+            case commands.MissingRequiredArgument():
+                await ctx.send(
+                    f"Not all required inputs were given for {ctx.command}!\n"
+                    f"For more information type `.help {ctx.command}`"
+                )
+            case commands.ExpectedClosingQuoteError():
+                await ctx.send(
+                    f"A quote was missing for command {ctx.command}!\n"
+                    f"For more information type `.help {ctx.command}`"
+                )
+            case commands.CommandInvokeError():
+                await self.handle_invoke_error(err.__cause__, ctx)
+            case _:
+                await ctx.send(
+                    f"An unhandled error occured when invoking {ctx.command}!\n"
+                    f"Error: {str(err)}\n"
+                    f"Type: {type(err)}\n"
+                    f"For more information type `.help {ctx.command}`\n"
+                )
 
-        if isinstance(err, commands.DisabledCommand):
-            await ctx.send(f"Sorry, {ctx.command} is currently disabled.\n")
-
-        elif isinstance(err, commands.MissingPermissions):
-            await ctx.send(
-                f"Sorry, you do not have the required permissions for {ctx.command}.\n"
-            )
-
-        elif isinstance(err, commands.MissingRole):
-            await ctx.send(
-                f"Sorry, you do not have the required role for {ctx.command}.\n"
-            )
-
-        elif isinstance(err, commands.MissingAnyRole):
-            await ctx.send(
-                f"Sorry, you do not have all the required roles for {ctx.command}.\n"
-            )
-
-        elif isinstance(err, commands.BadArgument):
-            await ctx.send(
-                f"Invalid inputs were supplied for {ctx.command}!\n"
-                f"For more information type `.help {ctx.command}`"
-            )
-
-        elif isinstance(err, commands.MissingRequiredArgument):
-            await ctx.send(
-                f"Not all required inputs were given for {ctx.command}!\n"
-                f"For more information type `.help {ctx.command}`"
-            )
-
-        elif isinstance(err, commands.ExpectedClosingQuoteError):
-            await ctx.send(
-                f"A quote was missing for command {ctx.command}!\n"
-                f"For more information type `.help {ctx.command}`"
-            )
-
-        elif isinstance(err.__cause__, ValueError) and ctx.command.name == "register":
+    async def handle_invoke_error(self, cause, ctx):
+        if isinstance(cause, ValueError) and ctx.command.name == "register":
             await ctx.send("Already registered!")
 
-        elif isinstance(err.__cause__, KeyError) and ctx.command.name == "log":
+        elif isinstance(cause, KeyError) and ctx.command.name == "log":
             await ctx.send("Not registered! `.register`")
 
-        elif isinstance(err.__cause__, ValueError) and ctx.command.name == "log":
-            await ctx.send(f"Error: {str(err.__cause__)}")
+        elif isinstance(cause, ValueError) and ctx.command.name == "log":
+            await ctx.send(f"Error: {str(cause)}")
 
-        elif (
-            isinstance(err.__cause__, ValueError) and ctx.command.name == "leaderboard"
-        ):
-            await ctx.send(f"Error: {str(err.__cause__)}")
+        elif isinstance(cause, ValueError) and ctx.command.name == "leaderboard":
+            await ctx.send(f"Error: {str(cause)}")
 
-        elif isinstance(err.__cause__, AttributeError) and ctx.command.name == "info":
+        elif isinstance(cause, AttributeError) and ctx.command.name == "info":
             await ctx.send("Not registered! `.register`")
 
-        elif isinstance(err.__cause__, ValueError) and ctx.command.name == "buy":
-            await ctx.send(f"Error: {str(err.__cause__)}")
+        elif isinstance(cause, ValueError) and ctx.command.name == "buy":
+            await ctx.send(f"Error: {str(cause)}")
 
-        elif isinstance(err.__cause__, KeyError) and ctx.command.name == "buy":
+        elif isinstance(cause, KeyError) and ctx.command.name == "buy":
             await ctx.send("Not registered! `.register`")
-
-        elif isinstance(err, commands.CommandInvokeError):
-            await ctx.send(
-                f"An error occured when invoking {ctx.command}!\n"
-                f"Error: {str(err)}\n"
-                f"Type: {type(err)}\n"
-                f"For more information type `.help {ctx.command}`\n"
-            )
 
         else:
             await ctx.send(
                 f"An unhandled error occured when invoking {ctx.command}!\n"
-                f"Error: {str(err)}\n"
-                f"Type: {type(err)}\n"
+                f"Error: {str(cause)}\n"
+                f"Type: {type(cause)}\n"
                 f"For more information type `.help {ctx.command}`\n"
             )
